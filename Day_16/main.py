f3005a79d00c1c1d33b88c3ee8564754cbe3b2a1
talib-ilahi <270878360+Talib-90@users.noreply.github.com>
2026-03-28 01:27:59 +0500

    Add new file day_16

--- Day_16/main.py
+++ Day_16/main.py
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 with open("Day_16/myFile.txt") as file:
     content = file.read()
     print(content)
@@ -9,5 +10,22 @@ with open("Day_16/myFile.txt") as file:
 # Append or add text in the file
 with open("Day_16/newFile.txt", "a") as data:
     data.write("\nLine added.")
+=======
+# file = open("Day_16/myFile.txt")
+# context = file.read()
+# print(context)
+
+with open("Day_16/myFile.txt") as file: # by default it's read only mode
+    repl = file.read()
+    print(repl.replace("Talib", "ali"))
+
+# with open("Day_16/myFile.txt", "a") as file: 
+#     file.write("\nNew Line added.")
+
+#Opening a File that doesn't exit in write mode will create it 
+# with open("Day_16/new_file.txt", "w") as file: 
+#     file.write("New Line added.")
+
+>>>>>>> cb04bf0 (Add new file day_16)
 
 file.close()
