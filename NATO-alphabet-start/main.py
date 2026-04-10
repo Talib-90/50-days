87562758230886311cfc462ba223dfdef3f34901
Talib-ilahi <270878360+Talib-90@users.noreply.github.com>
2026-04-10 20:04:22 +0500

    Modify code, Handle error

--- NATO-alphabet-start/main.py
+++ NATO-alphabet-start/main.py
@@ -1,17 +1,15 @@
 import pandas
-# Keyword Method with iterrows()
-# {new_key:new_value for (index, row) in df.iterrows()}
 
-# TODO 1. Create a dictionary in this format:
-# {"A": "Alfa", "B": "Bravo"}
-valueDict = {}
-nato_csv = pandas.read_csv("NATO-alphabet-start/nato_phonetic_alphabet.csv")
-# for index, row in nato_csv.iterrows():
-#     valueDict[row.letter] = row.code
+nato_csv = pandas.read_csv("nato_phonetic_alphabet.csv")
 
-# OR
 phonetic_data = {row.letter: row.code for index, row in nato_csv.iterrows()}
-# TODO 2. Create a list of the phonetic code words from a word that the user inputs.
-user_input = input("Enter a word: ").upper()
-result = [phonetic_data[letter] for letter in user_input]
-print(result)
+while True:
+    user_input = input("Enter a word: ").upper()
+    try:
+        result = [phonetic_data[letter] for letter in user_input]
+    except KeyError:
+        print("Sorry, Only letters in the alphabet please")
+        continue
+    else:
+        print(result)
+        break
